91df44bacc9646af77f9ca79c9fafe1c6064b565
Aliaksei <sladkiy.aleksey@gmail.com>
2023-03-25 14:23:08 +0100

    feat: added logic, template, styles in search-input component

--- youTubeClientApp/src/app/components/search-input/search-input.component.ts
+++ youTubeClientApp/src/app/components/search-input/search-input.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.scss'],
 })
-export class SearchInputComponent {}
+export class SearchInputComponent {
+  searchForm = new FormControl('Angular');
+
+  areSettingsVisible = false;
+
+  searchFormValue = '';
+
+  @Output() emitterSettingsVisible = new EventEmitter<boolean>();
+
+  @Output() emitterSearchFormValue = new EventEmitter<string>();
+
+  changeSettingsVisibility() {
+    this.areSettingsVisible = !this.areSettingsVisible;
+    this.emitterSettingsVisible.emit(this.areSettingsVisible);
+  }
+
+  getFormValue() {
+    if (this.searchForm.value) {
+      this.searchFormValue = this.searchForm.value;
+    }
+    this.searchForm.setValue('');
+    this.emitterSearchFormValue.emit(this.searchFormValue);
+  }
+}
