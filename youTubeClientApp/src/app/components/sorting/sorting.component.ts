63679251844bd4586cc12caa8a31c8b8c7f9fb3b
Aliaksei <sladkiy.aleksey@gmail.com>
2023-03-25 14:28:15 +0100

    feat: added logic, template, styles in sorting component

--- youTubeClientApp/src/app/components/sorting/sorting.component.ts
+++ youTubeClientApp/src/app/components/sorting/sorting.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sorting',
   templateUrl: './sorting.component.html',
   styleUrls: ['./sorting.component.scss'],
 })
-export class SortingComponent {}
+export class SortingComponent {
+  filterWord = '';
+
+  @Output() emitterFilterSearch = new EventEmitter<string>();
+
+  @Output() emitterFilterDate = new EventEmitter();
+
+  @Output() emitterFilterViews = new EventEmitter();
+
+  filterSearch() {
+    this.emitterFilterSearch.emit(this.filterWord);
+  }
+
+  filterDate() {
+    this.emitterFilterDate.emit();
+  }
+
+  filterCountOfViews() {
+    this.emitterFilterViews.emit();
+  }
+}
