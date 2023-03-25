410f8083ca341cad1cff78c443b8e13488a56c3e
Aliaksei <sladkiy.aleksey@gmail.com>
2023-03-25 14:29:00 +0100

    feat: added logic, template, styles in search-results component

--- youTubeClientApp/src/app/components/search-results/search-results.component.ts
+++ youTubeClientApp/src/app/components/search-results/search-results.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+
+import { IItem } from '../../models/search-item.model';
+import { response } from '../../response/mock-response';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
 })
-export class SearchResultsComponent {}
+export class SearchResultsComponent implements OnChanges {
+  @Input() searchFormValue = '';
+
+  @Input() filterWord = '';
+
+  @Input() isDateIncreasing: boolean | null;
+
+  @Input() isCountOfViewsIncreasing: boolean | null;
+
+  youTubeResponse: IItem[];
+
+  ngOnChanges() {
+    if (this.searchFormValue) {
+      this.youTubeResponse = response.items.filter((item) =>
+        item.snippet.title
+          .toLowerCase()
+          .includes(this.searchFormValue.toLowerCase())
+      );
+    } else {
+      this.youTubeResponse = [];
+    }
+  }
+}
