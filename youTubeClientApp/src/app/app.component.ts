27c42a57aa3500257eae2995964d88da173b2475
Aliaksei <sladkiy.aleksey@gmail.com>
2023-03-25 14:27:22 +0100

    feat: added logic, template, styles in app component

--- youTubeClientApp/src/app/app.component.ts
+++ youTubeClientApp/src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'youTubeClientApp';
+
+  searchFormValue = '';
+
+  filterWord = '';
+
+  isDateIncreasing: boolean | null = null;
+
+  isCountOfViewsIncreasing: boolean | null = null;
+
+  getSearchFormValue(value: string) {
+    this.searchFormValue = value;
+    this.isDateIncreasing = null;
+    this.isCountOfViewsIncreasing = null;
+  }
+
+  filterSearch(value: string) {
+    this.filterWord = value;
+  }
+
+  filterDate() {
+    this.isCountOfViewsIncreasing = null;
+    if (this.isDateIncreasing === null) {
+      this.isDateIncreasing = true;
+    } else {
+      this.isDateIncreasing = !this.isDateIncreasing;
+    }
+  }
+
+  filterCountOfViews() {
+    this.isDateIncreasing = null;
+    if (this.isCountOfViewsIncreasing === null) {
+      this.isCountOfViewsIncreasing = true;
+    } else {
+      this.isCountOfViewsIncreasing = !this.isCountOfViewsIncreasing;
+    }
+  }
 }
