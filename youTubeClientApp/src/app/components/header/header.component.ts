7d6152620fdd899956a895db7b464b0039bfd6df
Aliaksei <sladkiy.aleksey@gmail.com>
2023-03-25 14:25:28 +0100

    feat: added logic, template, styles in header component

--- youTubeClientApp/src/app/components/header/header.component.ts
+++ youTubeClientApp/src/app/components/header/header.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  areSettingsVisible = false;
+
+  @Output() emitterSearchFormValue = new EventEmitter<string>();
+
+  @Output() emitterFilterSearch = new EventEmitter<string>();
+
+  @Output() emitterFilterDate = new EventEmitter();
+
+  @Output() emitterFilterCountOfViews = new EventEmitter();
+
+  changeSettingsVisibility(areVisible: boolean) {
+    this.areSettingsVisible = areVisible;
+    console.log(this.areSettingsVisible);
+  }
+
+  getSearchFormValue(value: string) {
+    this.emitterSearchFormValue.emit(value);
+  }
+
+  filterSearch(value: string) {
+    this.emitterFilterSearch.emit(value);
+  }
+
+  filterDate() {
+    this.emitterFilterDate.emit();
+  }
+
+  filterCountOfViews() {
+    this.emitterFilterCountOfViews.emit();
+  }
+}
