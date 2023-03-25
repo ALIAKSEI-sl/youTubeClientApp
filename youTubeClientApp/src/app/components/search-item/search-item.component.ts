49fbff2b2e0caac8cabd5ff9e9bdf7daa034b99a
Aliaksei <sladkiy.aleksey@gmail.com>
2023-03-25 14:29:16 +0100

    feat: added logic, template, styles in search-item component

--- youTubeClientApp/src/app/components/search-item/search-item.component.ts
+++ youTubeClientApp/src/app/components/search-item/search-item.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { IItem } from '../../models/search-item.model';
 
 @Component({
   selector: 'app-search-item',
   templateUrl: './search-item.component.html',
   styleUrls: ['./search-item.component.scss'],
 })
-export class SearchItemComponent {}
+export class SearchItemComponent {
+  @Input() itemResponse: IItem;
+}
