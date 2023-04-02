123832aabdec4fbe16e0c0e6cf005f5e99ea7f59
Aliaksei <sladkiy.aleksey@gmail.com>
2023-04-02 15:49:15 +0200

    refactor: move pipes

--- youTubeClientApp/src/app/youtube-module/pipes/filter-search.pipe.ts
+++ youTubeClientApp/src/app/youtube-module/pipes/filter-search.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { IItem } from '../../../shared-module/models/search-item.model';
+import { IItem } from '../models/search-item.model';
 
 @Pipe({
   name: 'filterSearch',
