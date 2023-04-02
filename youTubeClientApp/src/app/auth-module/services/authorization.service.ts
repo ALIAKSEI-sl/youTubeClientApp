f7dd766bb6e982600110a5e25dd83d577b36e128
Aliaksei <sladkiy.aleksey@gmail.com>
2023-04-02 15:45:44 +0200

    feat: add Authorization Service

---
+++ youTubeClientApp/src/app/auth-module/services/authorization.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthorizationService {
+  userName = 'You Name';
+
+  isButtonVisible = false;
+
+  constructor(private router: Router) {}
+
+  checkLogIn() {
+    const token = localStorage.getItem('userToken');
+    const name = localStorage.getItem('userName');
+    if (token && name) {
+      this.isButtonVisible = !this.isButtonVisible;
+      this.userName = name;
+    }
+  }
+
+  changeName(name: string) {
+    this.userName = name;
+    localStorage.setItem('userName', name);
+  }
+
+  logOut() {
+    localStorage.removeItem('userToken');
+    this.userName = 'You Name';
+    this.router.navigateByUrl('/auth');
+    this.isButtonVisible = !this.isButtonVisible;
+  }
+
+  changeButtonVisibility() {
+    this.isButtonVisible = !this.isButtonVisible;
+  }
+
+  logIn() {
+    const token = 'eyJhbGciOiJIUzUxMiIsI';
+    localStorage.setItem('userToken', token);
+    this.router.navigateByUrl('/main');
+  }
+}
