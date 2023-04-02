abad7fb598c6f8c207c620250ea6e2a2b444f965
Aliaksei <sladkiy.aleksey@gmail.com>
2023-04-02 16:19:51 +0200

    feat: add Login Pages Component

---
+++ youTubeClientApp/src/app/auth-module/components/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { AuthorizationService } from '../../services/authorization.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  userLogin = '';
+
+  userPassword = '';
+
+  isLoginOrPasswordEmpty = false;
+
+  constructor(public authorizationService: AuthorizationService) {}
+
+  logIn() {
+    if (this.userLogin && this.userPassword) {
+      this.authorizationService.logIn();
+      this.authorizationService.changeName(this.userLogin);
+      this.authorizationService.changeButtonVisibility();
+    } else {
+      this.isLoginOrPasswordEmpty = true;
+    }
+  }
+}
---
+++ youTubeClientApp/src/app/auth-module/pages/login-pages/login-pages.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-pages',
+  templateUrl: './login-pages.component.html',
+  styleUrls: ['./login-pages.component.scss'],
+})
+export class LoginPagesComponent {}
