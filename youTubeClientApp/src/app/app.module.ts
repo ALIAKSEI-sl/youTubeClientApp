1f14333cec834ca2d9a6e5ab9988f920cb4c8272
Aliaksei <sladkiy.aleksey@gmail.com>
2023-04-08 21:06:51 +0200

    feat: add http Interceptor

--- youTubeClientApp/src/app/app.module.ts
+++ youTubeClientApp/src/app/app.module.ts
@@ -1,13 +1,18 @@
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core-module/core.module';
+import { UrlInterceptor } from './youtube-module/interceptors/url.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: UrlInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
