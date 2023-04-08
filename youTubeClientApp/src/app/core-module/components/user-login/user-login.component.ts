4b4b851b4d5b4881cf9c9c55897f8052da7975ce
Aliaksei <sladkiy.aleksey@gmail.com>
2023-04-08 21:11:18 +0200

    feat: add Observable in user-login component

--- youTubeClientApp/src/app/core-module/components/user-login/user-login.component.ts
+++ youTubeClientApp/src/app/core-module/components/user-login/user-login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
+import { Subscription } from 'rxjs';
 import { AuthorizationService } from 'src/app/auth-module/services/authorization.service';
 
 @Component({
@@ -7,14 +8,35 @@ import { AuthorizationService } from 'src/app/auth-module/services/authorization
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.scss'],
 })
-export class UserLoginComponent implements OnInit {
+export class UserLoginComponent implements OnInit, OnDestroy {
   constructor(public authorizationService: AuthorizationService) {}
 
+  isLogged!: boolean;
+
+  userName!: string;
+
+  subscriptionOnUserName!: Subscription;
+
+  subscriptionOnIsLogged!: Subscription;
+
   ngOnInit(): void {
+    this.subscriptionOnUserName = this.authorizationService.userName.subscribe(
+      (name) => (this.userName = name)
+    );
     this.authorizationService.checkLogIn();
+    this.subscriptionOnIsLogged = this.authorizationService.isLogged.subscribe(
+      (boolean) => (this.isLogged = boolean)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionOnUserName.unsubscribe();
+    this.subscriptionOnIsLogged.unsubscribe();
   }
 
   logOut() {
-    this.authorizationService.logOut();
+    if (this.isLogged) {
+      this.authorizationService.logOut();
+    }
   }
 }
