e1f1beb4c8ca903b0740f1f4291cd54f5a56c30b
Aliaksei <sladkiy.aleksey@gmail.com>
2023-04-08 21:08:55 +0200

    feat: add Http Client

--- youTubeClientApp/src/app/youtube-module/services/you-tube-response.service.ts
+++ youTubeClientApp/src/app/youtube-module/services/you-tube-response.service.ts
@@ -1,8 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { IItem } from 'src/app/youtube-module/models/search-item.model';
-
-import { response } from '../../youtube-module/response/mock-response';
+import { Observable, switchMap } from 'rxjs';
+import {
+  IItem,
+  IItemSearch,
+} from 'src/app/youtube-module/models/search-item.model';
+import {
+  IResponse,
+  IResponseSearch,
+} from 'src/app/youtube-module/models/search-response.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +17,20 @@ import { response } from '../../youtube-module/response/mock-response';
 export class YouTubeResponseService {
   youTubeResponse: IItem[] = [];
 
-  sendRequest(searchTerm: string) {
-    if (searchTerm) {
-      this.youTubeResponse = response.items.filter((item) =>
-        item.snippet.title.toLowerCase().includes(searchTerm)
-      );
-    } else {
-      this.youTubeResponse = [];
-    }
+  constructor(private http: HttpClient) {}
+
+  getResult(searchTerm: string): Observable<IResponse> {
+    const url = `search?type=video&part=snippet&maxResults=20&q=${searchTerm}`;
+    return this.http.get<IResponseSearch>(url).pipe(
+      switchMap((req: IResponseSearch) => {
+        const listId = req.items
+          .map((item: IItemSearch) => item.id.videoId)
+          .join(',');
+        const items = this.http.get<IResponse>(
+          `videos?id=${listId}&part=snippet,statistics`
+        );
+        return items;
+      })
+    );
   }
 }
