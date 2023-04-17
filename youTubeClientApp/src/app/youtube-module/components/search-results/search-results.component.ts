2b5eef8f2003435d8ed195b7cf689a40bfb24342
Aliaksei <sladkiy.aleksey@gmail.com>
2023-04-17 19:05:22 +0200

    feat: add store in Search Results Component

--- youTubeClientApp/src/app/youtube-module/components/search-results/search-results.component.ts
+++ youTubeClientApp/src/app/youtube-module/components/search-results/search-results.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
+import { Observable, Subscription } from 'rxjs';
+import {
+  selectCustomCards,
+  selectYouTubeResponse,
+} from 'src/app/redux/selectors/customCards.selector';
+import { ICustomCard } from 'src/app/redux/state.models';
+
+import { IItem } from '../../models/search-item.model';
 import { FilteringResultService } from '../../services/filtering-result.service';
-import { YouTubeResponseService } from '../../services/you-tube-response.service';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
 })
-export class SearchResultsComponent {
+export class SearchResultsComponent implements OnInit, OnDestroy {
+  customCard!: Observable<ICustomCard[]>;
+
+  youTubeResponse!: IItem[];
+
+  youTubeResponse$!: Subscription;
+
   constructor(
-    public youTubeResponseService: YouTubeResponseService,
-    public filteringResultService: FilteringResultService
+    public filteringResultService: FilteringResultService,
+    private store: Store
   ) {}
+
+  ngOnInit(): void {
+    this.customCard = this.store.select(selectCustomCards);
+    this.youTubeResponse$ = this.store
+      .select(selectYouTubeResponse)
+      .subscribe((response) => (this.youTubeResponse = response));
+  }
+
+  ngOnDestroy(): void {
+    this.youTubeResponse$.unsubscribe();
+  }
 }
